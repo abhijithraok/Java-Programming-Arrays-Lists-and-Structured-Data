84b15f634195ea1be501672e607158c859725999
Abhijith Rao <abijithraok@gmail.com>
2017-06-13 23:45:58 +0530

    added hashmap

--- Week 1/Word Lengths/src/wordLengths.java
+++ Week 1/Word Lengths/src/wordLengths.java
@@ -8,16 +8,56 @@ import java.io.*;
 import java.util.*;
 
 public class wordLengths {
+    public static HashMap hm = new HashMap();
     public static void main(String[] args) throws Exception {
-    FileResource resource = new FileResource();
-    int [] counts = new int[31];
-    countWordLengths(resource,counts);
-    System.out.println(counts);
+        FileResource resource = new FileResource();
+        ArrayList<Integer> array = new ArrayList<Integer>();
+
+      //  int[] counts = new int[31];
+        countWordLengths(resource, array);
+        System.out.println(hm);
+       int max = indexOfMax(array);
+    System.out.print(max);
+    }
+
+    public static void countWordLengths(FileResource resource, ArrayList<Integer> array) {
+        for (String word : resource.words()) {
+            int wordLength = 0;
+            StringBuilder sb = new StringBuilder(word);
+            //  System.out.println(sb);
+            for (int i = 0; i < sb.length(); i++) {
+                if (!Character.isLetter(sb.charAt(i))) {
+                    sb.deleteCharAt(i);
+                }
+                //   else if(i ==sb.length()-1 && !Character.isLetter(sb.charAt(i))){
+                //    sb.deleteCharAt(i);
+                // }
+                else {
+                    wordLength++;
+                }
+
+            }
+            String result = sb.toString();
+            hm.put(result,wordLength);
+            array.add(wordLength++);
+           // System.out.println(result);
+
+
+        }
 
     }
 
-    public  static void countWordLengths(FileResource resource, int[] counts) {
+    public static  int indexOfMax(ArrayList<Integer> values) {
+        int max = 0;
+        for (int i = 0; i < values.size(); i++) {
+            if (values.get(i) >= max) {
+                max = values.get(i);
 
+            }
+
+        }
+        return max;
     }
 }
+
 
