dddec1a7f80423ce51338be37af2d24223fcb02b
Abhijith Rao <abijithraok@gmail.com>
2017-06-23 00:38:13 +0530

    method to print dna and getmostCommonCodon

--- Week 2/Codon Count/src/codonCount.java
+++ Week 2/Codon Count/src/codonCount.java
@@ -3,7 +3,6 @@
  */
 
 import edu.duke.*;
-
 import java.util.*;
 
 public class codonCount {
@@ -15,34 +14,75 @@ public class codonCount {
 
     public static void main(String[] args) {
         codonCount cc = new codonCount();
-        FileResource fr = new FileResource();
-        String dna = fr.asString();
-        cc.buildCodonMap(0, dna);
-        for (String w : cc.map.keySet()) {
-            int number = cc.map.get(w);
-            System.out.println(w + "\t" + number);
+        cc.tester();
+
+    }
+
+    public String getMostCommonCodeon() {
+        String maxValue = "";
+        int maxValueInMap = (Collections.max(map.values()));
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == maxValueInMap) {
+                maxValue = entry.getKey();
+
+            }
+        }
+        return maxValue;
+    }
+    public void printCodonCounts(int start, int end) {
+        for (String key : map.keySet()) {
+            int value = map.get(key);
+            if (value >= start && value <= end) {
+                System.out.println(key + "\t" + value);
+            }
         }
     }
 
     public void buildCodonMap(int start, String dna) {
+        map.clear();
         dna = dna.toUpperCase();
+        String currDna = "";
         int stopIndex = 0;
         while (stopIndex <= dna.length() - 3) {
             stopIndex = start + 3;
-            //  if(!(stopIndex >= dna.length()-2)) {
-            dna = dna.substring(start, stopIndex);
-            if (map.keySet().contains(dna)) {
-                map.put(dna, map.get(dna) + 1);
+            currDna = dna.substring(start, stopIndex);
+            if (map.keySet().contains(currDna)) {
+                map.put(currDna, map.get(currDna) + 1);
             } else {
-                map.put(dna, 1);
+                map.put(currDna, 1);
 
             }
+
             start += 3;
         }
-        //  else {
-        //   break;
+
     }
-    // start++;
+
+
+    public void tester() {
+        FileResource fr = new FileResource();
+        String dna = fr.asString().trim();
+        int start =1;
+        int end = 5;
+        buildCodonMap(0, dna);
+        String largest = getMostCommonCodeon();
+        System.out.println("Most common codon is "+largest+" with count "+map.get(largest)+"\t");
+        System.out.println("Counts of codons between "+start+" and "+end+" inclusive are:"+"\t");
+        printCodonCounts(start, end);
+
+
+        buildCodonMap(1, dna);
+        largest = getMostCommonCodeon();
+        System.out.println("\nMost common codon is "+largest+" with count "+map.get(largest)+"\t");
+        System.out.println("Counts of codons between "+start+" and "+end+" inclusive are:"+"\t");
+        printCodonCounts(start, end);
+
+        buildCodonMap(2, dna);
+        largest = getMostCommonCodeon();
+        System.out.println("\nMost common codon is "+largest+" with count "+map.get(largest)+"\t");
+        System.out.println("Counts of codons between "+start+" and "+end+" inclusive are:"+"\t");
+        printCodonCounts(start, end);
+    }
 }
 
 
