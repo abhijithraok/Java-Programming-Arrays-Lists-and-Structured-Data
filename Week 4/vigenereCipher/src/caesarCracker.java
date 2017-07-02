5706a8e5af0cce7342e9daa8022c1fbe7edb085e
Abhijith Rao <abijithraok@gmail.com>
2017-07-02 18:41:26 +0530

    initial commit

---
+++ Week 4/vigenereCipher/src/caesarCracker.java
@@ -0,0 +1,58 @@
+/**
+ * Created by Abhijith on 7/2/2017.
+ */
+import edu.duke.*;
+public class caesarCracker {
+    char mostCommon;
+
+    public caesarCracker() {
+        mostCommon = 'e';
+    }
+
+    public caesarCracker(char c) {
+        mostCommon = c;
+    }
+
+    public int[] countLetters(String message){
+        String alph = "abcdefghijklmnopqrstuvwxyz";
+        int[] counts = new int[26];
+        for(int k=0; k < message.length(); k++){
+            int dex = alph.indexOf(Character.toLowerCase(message.charAt(k)));
+            if (dex != -1){
+                counts[dex] += 1;
+            }
+        }
+        return counts;
+    }
+
+    public int maxIndex(int[] vals){
+        int maxDex = 0;
+        for(int k=0; k < vals.length; k++){
+            if (vals[k] > vals[maxDex]){
+                maxDex = k;
+            }
+        }
+        return maxDex;
+    }
+
+    public int getKey(String encrypted){
+        int[] freqs = countLetters(encrypted);
+        int maxDex = maxIndex(freqs);
+        int mostCommonPos = mostCommon - 'a';
+        int dkey = maxDex - mostCommonPos;
+        if (maxDex < mostCommonPos) {
+            dkey = 26 - (mostCommonPos-maxDex);
+        }
+        return dkey;
+    }
+
+    public String decrypt(String encrypted){
+        int key = getKey(encrypted);
+        caesarCipher cc = new caesarCipher(key);
+        return cc.decrypt(encrypted);
+
+    }
+
+}
+
+
---
+++ Week 4/vigenereCipher/src/vigenereBreaker.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Abhijith on 7/2/2017.
+ */
+import java.util.* ;
+public class vigenereBreaker {
+    public String sliceString(String message, int whichSlice, int totalSlices) {
+        //REPLACE WITH YOUR CODE
+        return "WRITE ME!";
+    }
+
+    public int[] tryKeyLength(String encrypted, int klength, char mostCommon) {
+        int[] key = new int[klength];
+        //WRITE YOUR CODE HERE
+        return key;
+    }
+
+    public void breakVigenere () {
+        //WRITE YOUR CODE HERE
+    }
+
+}
